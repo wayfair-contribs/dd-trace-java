be173398d4ada234370b64c48492dbc41e4d8346
Bruce Bujon <bruce.bujon@datadoghq.com>
2022-10-21 16:52:02 +0200

    feat: Split Config class to dedicated feature config
    Split main config class
    Split default values
    Keep Config API and delegate to new feature config

---
+++ internal-api/src/main/java/datadog/trace/api/config/CwsFeatureConfig.java
@@ -0,0 +1,27 @@
+package datadog.trace.api.config;
+
+import static datadog.trace.api.config.CwsConfig.CWS_ENABLED;
+import static datadog.trace.api.config.CwsConfig.CWS_TLS_REFRESH;
+import static datadog.trace.api.config.CwsConfig.DEFAULT_CWS_ENABLED;
+import static datadog.trace.api.config.CwsConfig.DEFAULT_CWS_TLS_REFRESH;
+
+import datadog.trace.bootstrap.config.provider.ConfigProvider;
+
+public class CwsFeatureConfig extends AbstractFeatureConfig {
+  private final boolean cwsEnabled;
+  private final int cwsTlsRefresh;
+
+  public CwsFeatureConfig(ConfigProvider configProvider) {
+    super(configProvider);
+    this.cwsEnabled = configProvider.getBoolean(CWS_ENABLED, DEFAULT_CWS_ENABLED);
+    this.cwsTlsRefresh = configProvider.getInteger(CWS_TLS_REFRESH, DEFAULT_CWS_TLS_REFRESH);
+  }
+
+  public boolean isCwsEnabled() {
+    return this.cwsEnabled;
+  }
+
+  public int getCwsTlsRefresh() {
+    return this.cwsTlsRefresh;
+  }
+}
